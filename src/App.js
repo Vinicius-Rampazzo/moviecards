bc3bce57ff91b0c2b99737acb9e2a9b1911c21fe
vinicius-rampazzo <vinicius_rampazzo10@hotmail.com>
2025-02-04 11:38:11 -0300

    iniciando alteraoes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header';
-import MovieList from './components/MovieList';
-import Footer from './components/Footer';
-import movies from './data';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import MovieList from "./components/MovieList";
+import Footer from "./components/Footer";
+import movies from "./data";
+// iniciando alterações
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <MovieList movies={ movies } />
+      <MovieList movies={movies} />
       <Footer />
     </div>
   );
